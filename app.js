b60ce8bddb58e29044c25407295e7df4b1de503f
talemanjan <talemanaptech@gmail.com>
2023-03-15 17:03:19 +0500

    added new js

--- app.js
+++ app.js
@@ -15,14 +15,7 @@ function check(){
         document.getElementById("aa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Does she own a car?";
         break;
 
-case "He and I went to the store.":
-    document.getElementById("aaa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"He and I went to the store.";
-
-break;
-
-
-
-      default:
+   default:
         document.getElementById("aa").innerHTML="The answer is wrong " +" "+"here are some of the ways to write it :"+"<br>"+q1[0]+"<br>"+q1[1]+"<br>"+q1[2]; //could been done with creating an li 
         break;
     } 
@@ -33,7 +26,7 @@ document.getElementById("aaa").innerHTML=" "
   let next=()=> {
 // document.getElementById("aa").innerHTML=" "
 document.getElementById("aaa").innerHTML="Correct it::  Me and him went to the store"
-
+document.getElementById("ans-btn").onclick=checkk;
 
   }
   
