7a1a058acd34451732ebbd899977af45b69ca2cb
talemanjan <talemanaptech@gmail.com>
2023-03-15 17:51:13 +0500

    added new js

--- app.js
+++ app.js
@@ -49,9 +49,7 @@ let next = () => {
     document.getElementById("aaa").innerHTML = "Correct it: Me and him went to the store";
     document.getElementById("ans-btn").onclick = checkk;
     document.getElementById("next-btn").remove();
-    const button = document.createElement("button"); 
-    button.innerText = "Answer Again"; 
-   
+
   }
 
 function checkkk(){
