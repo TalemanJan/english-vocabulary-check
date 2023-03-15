bf54fa91816aa7051600bbebd8ad50e1f98e9bbc
talemanjan <talemanaptech@gmail.com>
2023-03-15 16:43:25 +0500

    added new js

--- app.js
+++ app.js
@@ -28,34 +28,11 @@ break;
     } 
   }
 
-
+document.getElementById("aaa").innerHTML=" "
 
   let next=()=> {
-var inp_1 = document.getElementById("inp-1").value
-  switch (inp_1) {
-    case "Is she in possession of a car?":
-      document.getElementById("aa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Is she in possession of a car?"+"]";          
-      break;
-    case "Does she have a car?":
-      document.getElementById("aa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Does she have a car?";
-      break;
-    case "Does she own a car?":
-      document.getElementById("aa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Does she own a car?";
-      break;
-
-case "He and I went to the store.":
-  document.getElementById("aa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"He and I went to the store.";
-
-break;
-
-
-
-    default:
-      document.getElementById("aa").innerHTML="The answer is wrong " +" "+"here are the correct answers:"+"<br>"+q1[0]+"<br>"+q1[1]+"<br>"+q1[2]; //could been done with creating an li 
-      break;
-  }  ;
-
-
+document.getElementById("aa").innerHTML=" "
+document.getElementById("aaa").innerHTML="hello"
 
 
   }
