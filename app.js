a2d53eff2d8c5a1109d873f6e232edd6a4b96286
talemanjan <talemanaptech@gmail.com>
2023-03-15 16:20:51 +0500

    added new js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-var q1=[]
+var q1=["Does she own a car?","Does she have a car?","Is she in possession of a car?"]
 
 function check(){
     var inp_1 = document.getElementById("inp-1").value;
@@ -14,7 +14,7 @@ function check(){
         document.getElementById("aa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Does she own a car?";
         break;
       default:
-        document.getElementById("aa").innerHTML="That's wrong ";
+        document.getElementById("aa").innerHTML="That's wrong" +" "+"here are the correct answers:"+q1[0];
         break;
     } 
   }
