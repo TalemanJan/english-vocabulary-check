951060b3608e296f59978d9db5b4193b4a2bb586
talemanjan <talemanaptech@gmail.com>
2023-03-15 17:34:22 +0500

    added new js

--- app.js
+++ app.js
@@ -51,11 +51,31 @@ let next = () => {
     document.getElementById("ans-btn").onclick = checkk;
   }
 
+function checkkk(){
+    var inp_1 = document.getElementById("inp-1").value;
+    switch (inp_1) {
+      case "He and I went to the store.":
+        document.getElementById("aaa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Is she in possession of a car?"+"]";          
+        break;
+      case "Him and I went to the store.":
+        document.getElementById("aaa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Does she have a car?";
+        break;
+      case "He went to the store with me.":
+        document.getElementById("aaa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Does she own a car?";
+        break;
+
+   default:
+        document.getElementById("aaa").innerHTML="The answer is wrong " +" "+"here are some of the correct ways to write it :"+"<br>"+q2[0]+"<br>"+q2[1]+"<br>"+q2[2]; //could been done with creating an li 
+        break;
+    } 
+
+}
+
   let nexttt = () => {
   var abc="hello"
-    document.getElementById("aaa").innerHTML = "Correct it: They're going to the party too.";
-    document.getElementById("next-btn").onclick = abc;
-    // document.getElementById("ans-btn").onclick=checkkk;
+    document.getElementById("aaaa").innerHTML = "Correct it: They're going to the party too.";
+     document.getElementById("ans-btn").onclick=checkkk;
+     document.getElementById("next-btn").onclick=next;
   }
   
   
