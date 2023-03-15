33a3067cb253546ea89d33553e9b763293b81c10
talemanjan <talemanaptech@gmail.com>
2023-03-15 22:10:46 +0500

    added js

--- app.js
+++ app.js
@@ -52,17 +52,17 @@ function checkkk(){
     var inp_1 = document.getElementById("inp-1").value;
     switch (inp_1) {
       case "He and I went to the store.":
-        document.getElementById("aaa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Is she in possession of a car?"+"]";          
+        document.getElementById("aaaa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Is she in possession of a car?"+"]";          
         break;
       case "Him and I went to the store.":
-        document.getElementById("aaa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Does she have a car?";
+        document.getElementById("aaaa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Does she have a car?";
         break;
       case "He went to the store with me.":
-        document.getElementById("aaa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Does she own a car?";
+        document.getElementById("aaaa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Does she own a car?";
         break;
 
    default:
-        document.getElementById("aaa").innerHTML="The answer is wrong " +" "+"here are some of the correct ways to write it :"+"<br>"+"1:"+" "+q2[0]+"<br>"+"2:"+" "+q2[1]+"<br>"+" 3: "+q2[2]; //could been done with creating an li 
+        document.getElementById("aaaa").innerHTML="The answer is wrong " +" "+"here are some of the correct ways to write it :"+"<br>"+"1:"+" "+q2[0]+"<br>"+"2:"+" "+q2[1]+"<br>"+" 3: "+q2[2]; //could been done with creating an li 
         break;
     } 
 
