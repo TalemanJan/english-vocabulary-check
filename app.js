196a4bf6fde1f7ba6a36d1cae3a95912f54365df
talemanjan <talemanaptech@gmail.com>
2023-03-15 16:35:52 +0500

    added new js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
-var q1=["Does she own a car?","Does she have a car?","Is she in possession of a car?"]
 
+var q1=["Does she own a car?","Does she have a car?","Is she in possession of a car?"]
+var q2=["He and I went to the store.","Him and I went to the store.","He went to the store with me."]
 function check(){
     var inp_1 = document.getElementById("inp-1").value;
     switch (inp_1) {
@@ -13,12 +14,23 @@ function check(){
       case "Does she own a car?":
         document.getElementById("aa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"Does she own a car?";
         break;
+
+case "He and I went to the store.":
+    document.getElementById("aa").innerHTML = "Excellent work thats the correct answer"+" "+"[" +"He and I went to the store.";
+
+break;
+
+
+
       default:
         document.getElementById("aa").innerHTML="The answer is wrong " +" "+"here are the correct answers:"+"<br>"+q1[0]+"<br>"+q1[1]+"<br>"+q1[2]; //could been done with creating an li 
         break;
     } 
   }
 
+
+
+  let next=()=> ;
 
 
 
